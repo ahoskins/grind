08f716ba75c62d5cbdf37e26f780ecd438fe7ee3
Andrew Hoskins <andrew.hoskins16@gmail.com>
2015-09-19 14:54:02 -0400

    add heroku stuff

--- app.js
+++ app.js
@@ -8,12 +8,9 @@ var express = require('express'),
 // module.exports is what other files get if they require() this...so all of 'app' is exposed
 var app = module.exports = express();
 
-
 /**
  * Configuration
  */
-
-// all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -25,44 +22,21 @@ app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 
 var env = process.env.NODE_ENV || 'development';
-
-// // development only
-// if (env === 'development') {
-//   app.use(errorHandler());
-// }
-
-// var prod = false;
-// if (env === 'production') {
-//   prod = true;
-// }
-
-// app.use(function(req, res, next) {
-//   req.in_production = prod;
-//   next();
-// });
+if (env === 'development') {
+  app.use(errorHandler());
+}
 
 /**
 * Routes
 **/
-
-
-/**
-* Bare domain, render index.html
-* Render the partials received from $routeProvider
-* Any other domain render 404.html
- */
 app.get('/', function(req, res) {
 	res.render('index');
 });
-// app.get('/partials/:name', routes.partials);
-// app.use(routes.error);
-
 
 
 /**
  * Start Server
  */
-
 http.createServer(app).listen(app.get('port'), function (req, resp) {
   console.log('Express server listening on port ' + app.get('port'));
 });
